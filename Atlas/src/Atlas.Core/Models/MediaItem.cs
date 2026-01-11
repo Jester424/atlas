33054fa8894f82bccc8f98952373af1c4fb06e6a
Nicholas Nicosia <nnicosi@wgu.edu>
2026-01-10 20:02:57 -0800

    Added MediaItem class and MediaType enum

---
+++ Atlas/src/Atlas.Core/Models/MediaItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Atlas.Core.Models
+{
+    public class MediaItem
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+
+        public string Title { get; init; } = string.Empty;
+
+        public int? ReleaseYear { get; init; }
+
+        public MediaType Type { get; init; }
+    }
+}
---
+++ Atlas/src/Atlas.Core/Models/MediaType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atlas.Core.Models
+{
+    public enum MediaType
+    {
+        Movie,
+        TvShow,
+        Game,
+        Book
+    }
+}
