44360a5b29f55cd4e9753d0aaaf2f3b05ce0b2b8
Nicholas Nicosia <nnicosi@wgu.edu>
2026-01-10 20:43:54 -0800

    Add Response class

---
+++ Atlas/src/Atlas.Core/Models/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atlas.Core.Models
+{
+    public class Response
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+
+        public Guid MediaItemId {  get; init; }
+
+        public int Rating { get; init; }
+    }
+}
