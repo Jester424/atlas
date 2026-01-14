f100f7c9bb1ebcacd12cfcdcf06469dc0f83479c
Nicholas Nicosia <nnicosi@wgu.edu>
2026-01-13 17:51:04 -0800

    Added Likert Response enum

---
+++ Atlas/src/Atlas.Core/Domain/LikertResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atlas.Core.Domain
+{
+    public enum LikertResponse
+    {
+        StronglyDisagree = 0,
+        Disagree = 1,
+        Neutral = 2,
+        Agree = 3,
+        StronglyAgree = 4
+    }
+}
